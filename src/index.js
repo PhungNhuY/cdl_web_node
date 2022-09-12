16f021af40d43660e4bcb087c4b2f28c90b0a843
yphung282@gmail.com <yphung282@gmail.com>
2022-09-12 17:32:54 +0700

    center content for smalldevice

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { extname } = require('path')
 const path = require('path')
 
 const app = express()
-const port = 80
+const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
